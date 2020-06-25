aefa6935cf87477bdde8747e19530031b10d2186
alexkaunsss <60197967+alexkaunsss@users.noreply.github.com>
2020-06-25 16:44:41 +0300

    Edited own website
    +

---
+++ lucid/js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+  $('.feature-sliders').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    easing: 'ease',
+    responsive: [
+    
+    {
+      breakpoint: 992,
+      settings: {
+      slidesToShow: 2,
+      arrows: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      slidesToScroll: 2
+      }
+    },
+
+   {
+      breakpoint: 600,
+      settings: {
+      slidesToShow: 2,
+      arrows: true,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      easing: 'ease',
+      slidesToScroll: 1
+      }
+    },
+
+    {
+      breakpoint: 490,
+      settings: {
+      slidesToShow: 1,
+      arrows: true,
+      autoplay: true,
+      easing: 'ease',
+      autoplaySpeed: 2000,
+      slidesToScroll: 1
+      }
+    }
+]
+
+  });
+  
+
+});
